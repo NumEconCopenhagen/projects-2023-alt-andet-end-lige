7aefd97ec98030a11fefc392c2dd0dbcd384c3fb
asgerdalsjoe <hlq510@alumni.ku.dk>
2023-03-19 15:04:29 +0100

    Fik løst fejlen til opgave 1

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -73,25 +73,14 @@ class HouseholdSpecializationModelClass:
         # a. consumption of market goods
         C = par.wM*LM + par.wF*LF
 
-        # b. home production
-        #def H(self, HM, HF):
-            #y = ((1-par.alpha) * HM**((par.sigma-1)/par.sigma) + par.alpha * HF**((par.sigma-1)/par.sigma))**((par.sigma)/par.sigma-1) # function if else
-            #y[par.sigma == 0] = np.min(HM, HF) # function for sigma = 0
-            #y[par.sigma == 1] = HM**(1-par.alpha)*HF**par.alpha # function for sigma = 1
-            #return y
 
-        def H(self, HM, HF):
-            ''' Consumption of home produced goods'''
-            #a. Unpack
-            par = self.par
-        
-            #b. Return
-            if par.sigma == 0.: #minimum
-                return np.min(HM,HF)
-            elif par.sigma == 1.: #Cobb-Douglas
-                return HM**(1-par.alpha)*HF**par.alpha
-            else: #CES
-                return ((1-par.alpha)*HM**((par.sigma-1)/(par.sigma)) + par.alpha*HF**((par.sigma-1)/(par.sigma)))**(par.sigma/(par.sigma-1))
+        #b. Return
+        if par.sigma == 0.: #minimum
+            H = np.min(HM,HF)
+        elif par.sigma == 1.: #Cobb-Douglas
+            H = HM**(1-par.alpha)*HF**par.alpha
+        else: #CES
+            H = ((1-par.alpha)*HM**((par.sigma-1)/(par.sigma)) + par.alpha*HF**((par.sigma-1)/(par.sigma)))**(par.sigma/(par.sigma-1))
     
 
         # c. total consumption utility
