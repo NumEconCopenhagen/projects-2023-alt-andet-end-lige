1094ba54ec47807f9cd0bf1935e9b0dffe023562
MaltheVeje <qnx178@alumni.ku.dk>
2023-03-26 18:47:06 +0200

    wup

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -157,7 +157,7 @@ class HouseholdSpecializationModelClass:
 
         bounds = ((0,24),(0,24),(0,24),(0,24))
 
-        result = optimize.minimize(obj, intial_guess, constraints=constraints, method = "SLSQP", bounds=bounds)
+        result = optimize.minimize(obj, intial_guess, constraints=constraints, method = "Nelder-Mead", bounds=bounds)
         
         # Setting the solution equal to the solution namespace:
         opt.HM = sol.HM = result.x[1]
@@ -177,7 +177,7 @@ class HouseholdSpecializationModelClass:
         """ solve model for vector of female wages """
         par = self.par
         sol = self.sol
-        
+
         for i,wF in enumerate(par.wF_vec):
 
             self.par.wF = wF
