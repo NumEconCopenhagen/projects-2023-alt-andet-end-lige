87f7480653afa54e99e60989389119796d5aaedc
asgerdalsjoe <hlq510@alumni.ku.dk>
2023-05-08 16:56:43 +0200

    opskrevet funktioner

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,5 +1,21 @@
 from scipy import optimize
 
+def Rl(yl, el, alpha=0.5):
+    return yl + alpha*el
+    
+def Rh(yh, eh, alpha=0.5):
+    return yh + alpha*eh
+    
+def ul(w, bl, el):
+    return w-bl*el
+
+def uh(w, bh, eh):
+    return w-bh*eh
+
+def revenue_max(q, yh, alpha, eh, wh, yl, el, wl):
+    return q*(yh + alpha*eh - wh) + (1-q)*(yl+alpha*el-wl)
+
+
 def solve_ss(alpha, c):
     """ Example function. Solve for steady state k. 
 
