34f569da1aa116276e981388fa2b4712cbbb03ab
asgerdalsjoe <hlq510@alumni.ku.dk>
2023-05-25 11:49:50 +0200

    asg

--- examproject/exam_asg.py
+++ examproject/exam_asg.py
@@ -27,54 +27,54 @@ class LaborAdjustmentCosts():
 
         # solutions
         sol.lt = np.zeros(len(par.kappas))
+        sol.h = np.zeros(par.K)
 
 
         # extended model parameters
         par.rho = 0.90
         par.iota = 0.01
-        par.sigma = 0.10
+        par.sigma_epsilon = 0.10
         par.R = (1+0.01)**(1/12)
         par.kappam1 = 1.0
         par.lm1 = 0.0
         
-    
-    #def supply(lt, yt):
-      #  lt = yt
-      #  return yt
-    
-    #def demand(self, kappat, yt):
-       # pt = kappat * yt**(-self.par.eta)
-       # return pt
-       
+        # Simulation parameters
+        par.T = 120 # Planning horizon
+        par.K = 10000 # number of shocks to simulate
 
+    ############## Question 1 ################
     
-    def profits(self, kappat, lt):
-        profits = kappat * lt**(1-self.par.eta) - self.par.w * lt
+    def profits(self, kappat, lt): # defining the profits function
+        profits = kappat * lt**(1-self.par.eta) - self.par.w * lt #calculating profits given the formula
         return profits
     
-    def obj(self, kappat, x):
-        return -self.profits(kappat, x)
+    def obj(self, kappat, x): # defining the objective function
+        return -self.profits(kappat, x) # negative sign because we want to maximize profits using a minimizer
         
-    def find_optimal_lt(self):
+    def find_optimal_lt(self): # defining the function that finds the optimal lt for each value of kappa
         for i,kappat in enumerate(self.par.kappas): # for loop to iterate over the different values of kappa
-            # calculating optimal lt given our formula
             
-            obj = lambda x: self.obj(kappat, x)
+            obj = lambda x: self.obj(kappat, x) #calling the objective function
             
-            result = optimize.minimize_scalar(obj, bounds=(0, 2), method='bounded')
+            result = optimize.minimize_scalar(obj, bounds=(0, 2), method='bounded') #using the bounded method to find the optimal lt that maximizes profits
             
-            self.sol.lt[i] = result.x
+            self.sol.lt[i] = result.x #saving the optimal lt in the solution namespace
             
-            print(f'For kappa = {kappat:.1f} the optimal labor supply is lt = {self.sol.lt[i]:.2f} which yields profits of {self.profits(kappat, self.sol.lt[i]):.2f} \n')
+            print(f'For kappa = {kappat:.1f} the optimal labor supply is lt = {self.sol.lt[i]:.2f} which yields profits of {self.profits(kappat, self.sol.lt[i]):.2f} \n') #printing results
             
-            
-            
-            #optimal_lt = ((1-self.par.eta)*kappat/self.par.w)**(1/(self.par.eta))
-            #self.sol.lt = optimal_lt 
-            
-            #calculating profits given the optimal lt
-            #profits = self.profits(self, kappat)
-            #print(f'For kappa = {kappat:.1f}, optimal lt = {self.sol.lt:.2f}, profits = {profits:.2f}')
+    ############## Question 2 ################                    
+
+    def ex_ante_expected_value(self):
+        for k in range(self.par.K)
+        #initialize shocks series
+        epsilon = 
+
+    def demand_shock(self, t):
+        return self.par.R**(t) * self.par.kappam1 * self.sol.lt[-1]**(1-self.par.eta)
+    
+    
+    
+         
     
         
         
