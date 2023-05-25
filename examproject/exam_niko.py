0ca16c27edd70263e0e5530ddd768a98f75c2f09
nikolinefreja <frt671@alumni.ku.dk>
2023-05-25 14:32:05 +0200

    great

--- examproject/exam_niko.py
+++ examproject/exam_niko.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-class optimaltaxation: 
+class OptimalTaxation: 
     def __init__(self):
         """ setup model """
 
@@ -69,7 +69,7 @@ class optimaltaxation:
         if do_print:
             print(f'L = {sol.L:.2f}')
     
-    def solve_w_vec(self):
+    def solve_w_vec(self, do_print=False):
         """ solve model for different wage rates """
         par = self.par
         sol = self.sol
@@ -82,6 +82,60 @@ class optimaltaxation:
             par.w = w
             self.solve()
             sol.L_vec.append(sol.L)
+        
+        if do_print:
+            print(sol.L_vec)
+    
+    def plot_L(self):
+        """ plot labour supply as a function of wage rate """
+        par = self.par
+        sol = self.sol
+
+        #Solution for different wage rates 
+        self.solve_w_vec()  
+        
+        # Plot L as a function of wage rate
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.plot(par.w_vec, sol.L_vec)
+        ax.set_xlabel('Wage rate')
+        ax.set_ylabel('Hours worked')
+        plt.title(r'$L^{\star}(\tilde{w})$')
+        plt.grid(True)
+        plt.show()
     
     def ext_government(self): 
-        
+        """solve model for different government consumption"""
+       
+        par = self.par
+        sol = self.sol
+
+        # a. Define new government consumption
+        G = par.tau * par.w * sol.L * ((1 - par.tau) * par.w)
+        par.G = G
+
+        #b. solve model with new government consumption
+        self.solve() 
+    
+    def implied_G(self): 
+        """Calculate implied government consumption"""
+
+        par = self.par
+        sol = self.sol
+
+        return par.tau * par.w * sol.L * ((1 - par.tau) * par.w)
+    
+    
+    def plot_3d(self): 
+        """ plot 3d graph of utility function as a function of hours worked and implied government consumption for a grid of tau"""
+
+        par = self.par
+        sol = self.sol
+
+        taus = np.linspace(0,1,100)
+
+        utility = np.empty((100))
+
+        for tau in enumerate(taus):
+                utility[i] = self.ext_government(tau)
+
